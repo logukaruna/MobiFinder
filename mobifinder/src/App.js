b726c65fabf17189d3d4ab93ee1fd29d9969521c
logukaruna <logukaruna28@gmail.com>
2023-08-17 22:53:32 +0530

    Checking the branch

--- mobifinder/src/App.js
+++ mobifinder/src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          naveen nee mass uu ley....
         </p>
         <a
           className="App-link"
